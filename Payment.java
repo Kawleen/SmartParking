62d8a5b5b98b6807bc535d486a0dda0630f485e6
Kawleen <kawleen94@gmail.com>
2020-01-24 17:35:19 +0100

    Create Payment.java

---
+++ Payment.java
@@ -0,0 +1,7 @@
+public interface Payment{
+	
+	public Context payYourWay(Context context) throws RemoteException, ContextException;
+
+	public Context timeIsMoney(Context context) throws RemoteException, ContextException;
+
+ }
